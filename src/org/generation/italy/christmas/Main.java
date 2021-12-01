32bbe7bc58a0b9e0f9176802bb408d3b2f7e9509
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-12-01 17:46:24 +0100

    refined

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -7,38 +7,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		//creo lista
 		ArrayList<String> listaRegali;
 		listaRegali = new ArrayList<String>();
+		
 		Scanner scan = new Scanner(System.in);
-		boolean continuare = false;
-		while(!continuare) {
+		boolean continuare = true;
+		
+		//aggiunta elementi
+		while (continuare) {
 			System.out.print("Aggiungi elemento alla lista dei desideri: ");
 			String regalo = scan.nextLine();
 			listaRegali.add(regalo);
 			
-			boolean rispostoContinuare1 = false;
-			while (!rispostoContinuare1) {
-			System.out.println("La tua lista è contiene " + listaRegali.size() + " desideri.\nContinuare? (s/n)");
-				String rispostaContinuare = scan.nextLine();
-				if (rispostaContinuare.equals("n")) {
+			
+			//verifica se continuare o meno
+			boolean nonRispostoContinuare = true;
+			while (nonRispostoContinuare) {
+			System.out.println("La tua lista è contiene " + listaRegali.size() + " desideri\nContinuare? (s/n)");
+				String risposta = scan.nextLine();
+				
+				if (risposta.toLowerCase().equals("s")) {
+					nonRispostoContinuare = false;
+				} else if (risposta.toLowerCase().equals("n")) {
 					continuare = false;
-					rispostoContinuare1= true;
-				} else if (rispostaContinuare.equals("s")) {
-					continuare = true;
-					rispostoContinuare1 = true;
-				} else {
-					rispostoContinuare1 = false;
+					nonRispostoContinuare = false;
 				} 
 			}
 		}
+		//stampa elementi
 		scan.close();
-		System.out.println("Ecco la tua lista :");
-		for(String s : listaRegali) {
-		System.out.println(s);
-	}
-
-	
+		System.out.println("Ecco la tua lista:");
+			
+		for (String s : listaRegali) {
+			System.out.println(s);
+		}
+		
 
 	}
 
